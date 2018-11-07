46a4525e50afbd708ef09604bec7c6a71361bfa4
Kevin Christen <admin@purpl3.net>
2018-11-07 16:55:37 +0100

    Fix setup.sh folder

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ ln -sf ~/dotfiles/compton/compton.conf ~/.config/
 ln -sf ~/dotfiles/i3/config ~/.config/i3/
 ln -sf ~/dotfiles/i3blocks/i3blocks.conf ~/.config/
 mkdir -p ~/.i3/i3scripts
-ln -sf ~/dotfiles/i3scripts/* /.i3/i3scripts/
+ln -sf ~/dotfiles/i3scripts/* ~/.i3/i3scripts/
 # Setup wallpapers
 cp ~/dotfiles/walls/* ~/Pictures/
